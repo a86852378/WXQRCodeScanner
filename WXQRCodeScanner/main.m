cd814a52df1a6211cea8602e91c1086369e4bde9
vison <vison_huang@intretech.com>
2018-04-02 15:38:26 +0800

    Initial Commit

---
+++ WXQRCodeScanner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WXQRCodeScanner
+//
+//  Created by vison on 2018/4/2.
+//  Copyright © 2018年 vison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
